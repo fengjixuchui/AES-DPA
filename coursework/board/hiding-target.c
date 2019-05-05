a21c66574363238b63e4042dba4942bf0c0bf649
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2019-05-05 22:54:16 +0100

    Cleaned up hiding-target.c and added timing code.

--- coursework/board/hiding-target.c
+++ coursework/board/hiding-target.c
@@ -7,6 +7,7 @@
 
 #include "encrypt.h"
 #include <stdlib.h>
+#include <time.h>
 
 #define AES_ENC_RND_MIX_STEP(a, b, c, d) { \
   aes_gf28_t __a1 = s[ a ];                \
@@ -27,60 +28,6 @@
   s[ d ] = __a3 ^ __b1 ^ __c1 ^ __d2;      \
 }
 
-#define AES_ENC_RND_INIT() { \
-  t_0 = rkp[0] ^ t_0;        \
-  t_1 = rkp[1] ^ t_1;        \
-  t_2 = rkp[1] ^ t_2;        \
-  t_3 = rkp[1] ^ t_3;        \
-  rkp += Nb;                 \
-}
-
-#define AES_ENC_RND_ITER() {                                    \
-  t_4 = rkp[ 0 ] ^ ( AES_ENC_TBOX_0[ ( t_0 >>  0 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_1[ ( t_1 >>  8 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_2[ ( t_2 >> 16 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_3[ ( t_3 >> 24 ) & 0xFF ] ) ; \
-  t_5 = rkp[ 1 ] ^ ( AES_ENC_TBOX_0[ ( t_1 >>  0 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_1[ ( t_2 >>  8 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_2[ ( t_3 >> 16 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_3[ ( t_0 >> 24 ) & 0xFF ] ) ; \
-  t_6 = rkp[ 2 ] ^ ( AES_ENC_TBOX_0[ ( t_2 >>  0 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_1[ ( t_3 >>  8 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_2[ ( t_0 >> 16 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_3[ ( t_1 >> 24 ) & 0xFF ] ) ; \
-  t_7 = rkp[ 3 ] ^ ( AES_ENC_TBOX_0[ ( t_3 >>  0 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_1[ ( t_0 >>  8 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_2[ ( t_1 >> 16 ) & 0xFF ] ) ^ \
-                   ( AES_ENC_TBOX_3[ ( t_2 >> 24 ) & 0xFF ] ) ; \
-  rkp += Nb; t_0 = t_4; t_1 = t_5; t_2 = t_6; t_3 = t_7;        \
-}
-
-#define AES_ENC_RND_FINI() {                                                 \
-  t_4 = rkp[ 0 ] ^ ( AES_ENC_TBOX_4[ ( t_0 >>  0 ) & 0xFF ] & 0x000000FF ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_1 >>  8 ) & 0xFF ] & 0x0000FF00 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_2 >> 16 ) & 0xFF ] & 0x00FF0000 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_3 >> 24 ) & 0xFF ] & 0xFF000000 ) ; \
-  t_5 = rkp[ 1 ] ^ ( AES_ENC_TBOX_4[ ( t_1 >>  0 ) & 0xFF ] & 0x000000FF ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_2 >>  8 ) & 0xFF ] & 0x0000FF00 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_3 >> 16 ) & 0xFF ] & 0x00FF0000 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_0 >> 24 ) & 0xFF ] & 0xFF000000 ) ; \
-  t_6 = rkp[ 2 ] ^ ( AES_ENC_TBOX_4[ ( t_2 >>  0 ) & 0xFF ] & 0x000000FF ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_3 >>  8 ) & 0xFF ] & 0x0000FF00 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_0 >> 16 ) & 0xFF ] & 0x00FF0000 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_1 >> 24 ) & 0xFF ] & 0xFF000000 ) ; \
-  t_7 = rkp[ 3 ] ^ ( AES_ENC_TBOX_4[ ( t_3 >>  0 ) & 0xFF ] & 0x000000FF ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_0 >>  8 ) & 0xFF ] & 0x0000FF00 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_1 >> 16 ) & 0xFF ] & 0x00FF0000 ) ^ \
-                   ( AES_ENC_TBOX_4[ ( t_2 >> 24 ) & 0xFF ] & 0xFF000000 ) ; \
-  rkp += Nb; t_0 = t_4; t_1 = t_5; t_2 = t_6; t_3 = t_7;                     \
-}
-
-aes_gf28_col_t AES_ENC_TBOX_0[256];
-aes_gf28_col_t AES_ENC_TBOX_1[256];
-aes_gf28_col_t AES_ENC_TBOX_2[256];
-aes_gf28_col_t AES_ENC_TBOX_3[256];
-aes_gf28_col_t AES_ENC_TBOX_4[256];
-
 aes_gf28_t rc[16] = {0x01, 0x02, 0x04, 0x08, 0x10,
                      0x20, 0x40, 0x80, 0x1B, 0x36};
 
@@ -96,7 +43,19 @@ int main(int argc, char* argv[]) {
 
   uint8_t t[16];
 
-  aes_enc(t, m, k);
+  int runs = 50000;
+  double time = 0;
+
+  for (int i = 0; i < runs; i++) {
+    clock_t tic = clock();
+    aes_enc(t, m, k);
+    clock_t toc = clock();
+    time += ((double) (toc-tic)) / CLOCKS_PER_SEC;
+  }
+
+  time /= runs;
+
+  printf("average runtime: %lf\n", time);
 
   if (!memcmp(t, c, 16 * sizeof(uint8_t))) {
     printf("AES.Enc( k, m ) == c\n");
@@ -202,10 +161,10 @@ void shuffle(int *array, size_t n) {
 }
 
 void aes_enc_rnd_sub(aes_gf28_t* s) {
-  int r[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-  shuffle(r, 16);
+  // int r[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+  // shuffle(r, 16);
   for (int i = 0; i < 16; i++) {
-    s[ r[i] ] = sbox(s[ r[i] ]);
+    s[i] = sbox(s[i]);
   }
 }
 
